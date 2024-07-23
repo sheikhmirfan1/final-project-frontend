8fd7b228626c86d6b990f462649bc5dbd8f1a2b6
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-07-23 13:45:04 +0200

    api fetch from backend

--- src/Pagecard/Card.jsx
+++ src/Pagecard/Card.jsx
@@ -1,28 +1,32 @@
 
 import PropertyCard from "./PropertyCard.jsx";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const Card = () => {
-  const properties = [
-    {
-      image:
-        "https://cdn.builder.io/api/v1/image/assets/TEMP/6757431bad4c2bbb79d5e0eb971cd15d1c98d192ae5230d75ce422b8d9969f30?apiKey=19e7bba656cf4334ab650b33c0aad2c0&",
-      title: "Luxury 2 bed flat, The Park",
-      price: "$1,800 per month",
-    },
-    {
-      image:
-        "https://cdn.builder.io/api/v1/image/assets/TEMP/7e88f7f569aa5b0a4396e7b27136de207879e6f460e411410155cbfe58c8e38a?apiKey=19e7bba656cf4334ab650b33c0aad2c0&",
-      title: "3 bed semi-detached house, Lenton",
-      price: "$1,500 per month",
-    },
-    {
-      image:
-        "https://cdn.builder.io/api/v1/image/assets/TEMP/3924d98905e73e88944a959a68edc65994fe222163521bfde585274c31601c87?apiKey=19e7bba656cf4334ab650b33c0aad2c0&",
-      title: "4 bed detached house, Beeston",
-      price: "$1,600 per month",
-    },
-  ];
+  const [products,setProducts] = useState(null);
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState(null);
+
+  const fetchProducts = async () => {
+    try {
+      const products = await axios.get('http://localhost:3000/api/products');
+      setProducts(products.data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+
+    finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+ 
 
   return (
     <section className="flex flex-col justify-center p-4 max-md:max-w-full">
@@ -31,9 +35,8 @@ const Card = () => {
       </h3>
       <div className="px-px max-md:max-w-full">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-          {properties.map((property, index) => (
-            <PropertyCard key={index} {...property} />
-          ))}
+        {products && !loading ? (products.map((product) => (
+          <PropertyCard key={product.id} {...product} />))) : 'Loading...'}
         </div>
       </div>
     </section>
--- src/Pagecard/PropertyCard.jsx
+++ src/Pagecard/PropertyCard.jsx
@@ -1,22 +1,30 @@
 
 
-const PropertyCard = ({ image, title, price }) => (
-  <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+import PropTypes from 'prop-types';
+
+const PropertyCard = ({name, description, price, image}) => {
+  
+  
+  return (
+  <div className="flex flex-col w-[50%] max-md:ml-0 max-md:w-full">
     <div className="flex flex-col grow self-stretch pb-3 leading-[150%] max-md:mt-3">
       <img
         loading="lazy"
         src={image}
         className="w-full aspect-[1.79]"
-        alt={title}
+        alt={name}
       />
       <div className="flex flex-col mt-3">
         <h4 className="justify-center text-base font-medium text-stone-100">
-          {title}
+          {name}
         </h4>
-        <p className="justify-center text-sm text-stone-100">{price}</p>
+        <p className="justify-center text-sm text-stone-100">
+          {description}</p>
+        <p className="justify-center text-sm text-stone-100">€{price}</p>
       </div>
     </div>
   </div>
-);
+)
+};
 
 export default PropertyCard;
