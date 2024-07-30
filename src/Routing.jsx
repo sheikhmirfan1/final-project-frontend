ec97a8a9f8c80a4e4096ea9c162d6674c5dff563
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-07-30 12:55:45 +0200

    added contactus components

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,21 +1,22 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import NavBar from "./components/Navbar.jsx";
-import OrderPage from "./orderfrom/OrderPage.jsx";
+import OrderPage from "./orderform/OrderPage.jsx";
+import ContactUs from "./contactUs/contactUs.jsx";
 
 const Routing = () => {
-    return (
-      <>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/order" element={<OrderPage />} />
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/contactUs" element={<ContactUs />} />
 
-          <Route path="*" element={<p>404 Not Found</p>} />
+        <Route path="*" element={<p>404 Not Found</p>} />
+      </Routes>
+    </>
+  );
+};
 
-        </Routes>
-      </>
-    );
-    }
-
-export default Routing
+export default Routing;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,16 +13,18 @@ const NavBar = () => {
       </div>
       <nav className="flex justify-between flex-1 gap-5 pl-20 max-md:flex-wrap ">
         <div className="flex gap-40 justify-between flex-wrap py-2.5 text-sm font-bold leading-5 whitespace-nowrap text-stone-100">
-          <Link to='/' className="justify-center">Home</Link>
-          <Link className="justify-center">Search</Link>
-          <Link to='/order' className="justify-center">Order</Link>
+          <Link to="/" className="justify-center">
+            Home
+          </Link>
+          <Link to="/order" className="justify-center">
+            Order
+          </Link>
+          <Link to="/contactUs" className="justify-center">Contact Us</Link>
           <Link className="justify-center">About</Link>
         </div>
         <div className="flex gap-2">
           <div className="flex flex-col justify-center px-4 py-2.5 text-sm font-bold leading-5 text-center bg-amber-500 rounded-xl w-100px text-stone-900">
-            <div className="justify-center text-ellipsis">
-              Orders list
-            </div>
+            <div className="justify-center text-ellipsis">Orders list</div>
           </div>
           <div className="flex flex-col justify-center items-center px-1.5 w-10 h-10 bg-rose-100 rounded-xl">
             <div className="flex justify-center items-center">
