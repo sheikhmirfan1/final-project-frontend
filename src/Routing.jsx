0dadf6767a6f9bdf29b62d2ea2768557f06b84ca
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-08-27 10:00:02 +0200

    footer fixed

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import {StickyNavbar} from "./components/Navbar.jsx";
-import OrderPage from "./orderForm/OrderPage.jsx";
+import OrderPage from "./orderForm/orderPage.jsx";
 import ContactUs from "./contactUs/contactUs.jsx";
 import About from "./About/aboutUs.jsx";
 import {SimpleRegistrationForm} from "./Reservation/booking.jsx";
-import {Register} from "./auth/register.jsx";
-import {LoginCard} from "./auth/signIn.jsx";
+// import {Register} from "./auth/register.jsx";
+// import {LoginCard} from "./auth/signIn.jsx";
 import  ShoppingCart  from "./cart/cart.jsx";
 
 
@@ -21,8 +21,8 @@ const Routing = () => {
         <Route path="/Reservation" element={<SimpleRegistrationForm />} />
         <Route path="/contactUs" element={<ContactUs />} />
         <Route path="/aboutUS" element={<About />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/signIn" element={<LoginCard />} />
+        {/* <Route path="/register" element={<Register />} />
+        <Route path="/signIn" element={<LoginCard />} /> */}
         <Route path="/cart" element={<ShoppingCart />} />
 
         <Route path="*" element={<p>404 Not Found</p>} />
