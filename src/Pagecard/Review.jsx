f12973566124698bacf1d970a63b24af7b643f68
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-08-27 11:51:11 +0200

    error fixed

--- src/Pagecard/Review.jsx
+++ src/Pagecard/Review.jsx
@@ -1,7 +1,9 @@
+
 import ReviewCard from "../About/ReviewCard.jsx";
 import img1 from "../assets/pic1.png";
 import img2 from "../assets/pic2.png";
 import img3 from "../assets/pic3.png";
+
 
 const Review = () => {
   return (
